5a3d2d2d265dcb1c3613da9e9eac4216d322f657
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-08-05 15:32:47 +0200

    Add routing

--- src/Form.container.ts
+++ src/Form.container.ts
@@ -1,6 +1,6 @@
 import {connect} from "react-redux"
 import {Dispatch} from "redux";
-import App from "./App";
+import Form from "./Form";
 import { State } from "./App.types";
 
 
@@ -18,4 +18,4 @@ export function mapDispatchToProps(dispatch: Dispatch) {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App.container";
-import {Provider} from "react-redux";
+import Form from "./Form.container";
+import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
 );
 
