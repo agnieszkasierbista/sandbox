66a6366a2a6a7d253723aef8be278057c827b2f7
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-08-25 11:39:25 +0200

    Test isString function

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,5 +1,10 @@
-import {findEmptyFields} from "./helpers"
+import {findEmptyFields, isString} from "./helpers"
 
 test('finds empty strings in touples and returns array of touple[0] for them', () => {
-  expect(findEmptyFields([["1", ""], ["2", "aaa"], ["3", "bbb"], ["4", ""], ["5", "ccc"] ])).toBe(["1", "4"]);
+  expect(findEmptyFields([["1", ""], ["2", "aaa"], ["3", "bbb"], ["4", ""], ["5", "ccc"] ])).toEqual(["1", "4"]);
 });
+
+test ('takes array of touples and evaluates if the second value of touple is a string', () => {
+    expect(isString([["1", ""], ["2", "aaa"], ["3", "bbb"], ["4", ""], ["5", "ccc"] ])).toBe(true);
+});
+
